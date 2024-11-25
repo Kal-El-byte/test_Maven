4bd905ae466d49d5aaaca087313a6cf7b092658f
Itachi Uchiha <danivetlyrics@gmail.com>
2024-11-25 20:23:26 +0100

    initiat commit

---
+++ src/main/java/ng/com/danivet/test_Maven/App.java
@@ -0,0 +1,10 @@
+package ng.com.danivet.test_Maven;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
